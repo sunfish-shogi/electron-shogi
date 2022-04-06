8904f91845a6c0eba9665197108ca65a2b97bc59
Kubo Ryosuke <sunfish-shogi@users.noreply.github.com>
2022-04-07 01:06:48 +0900

    migrate to module

--- archive.mjs
+++ archive.mjs
@@ -1,25 +1,24 @@
-'use strict'
+"use strict";
 
-const archiver = require("archiver");
-const fs = require("fs");
-const path = require("path");
+import archiver from "archiver";
+import fs from "fs";
 
 const platform = process.argv[2];
 const outputPath = `dist_electron/release-${platform}.zip`;
 
-const output = fs.createWriteStream(outputPath)
-  .on('error', function() {
-    throw err;
-  });
-const archive = archiver.create("zip")
-  .on('warning', function(err) {
-    if (err.code === 'ENOENT') {
+const output = fs.createWriteStream(outputPath).on("error", function () {
+  throw err;
+});
+const archive = archiver
+  .create("zip")
+  .on("warning", function (err) {
+    if (err.code === "ENOENT") {
       console.log(err);
     } else {
       throw err;
     }
   })
-  .on('error', function(err) {
+  .on("error", function (err) {
     throw err;
   });
 archive.pipe(output);
@@ -34,7 +33,9 @@ switch (platform) {
   default:
     throw new Error("unknown platform");
 }
-archive.file("docs/third-party-licenses.html", { name: "third-party-licenses.html" });
+archive.file("docs/third-party-licenses.html", {
+  name: "third-party-licenses.html",
+});
 archive.glob("third-party-licenses/*.txt", { cwd: "docs" });
 
 archive.finalize();

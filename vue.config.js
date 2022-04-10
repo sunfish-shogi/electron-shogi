faea09c472bfa1526dcf1e9ec765f05eedb65130
Kubo Ryosuke <sunfish-shogi@users.noreply.github.com>
2022-04-11 00:12:17 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.ts",
+      title: `Electron将棋 Version ${process.env.npm_package_version}`,
+    },
+  },
   outputDir: "docs/webapp",
   publicPath: "./",
   pluginOptions: {

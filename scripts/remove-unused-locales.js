e1eb50727f4c0f55e102598d662c4bf801288689
Kubo, Ryosuke <sunfish-shogi@users.noreply.github.com>
2022-12-17 04:02:59 +0900

    Remove unused local pack

--- scripts/remove-unused-locales.js
+++ scripts/remove-unused-locales.js
@@ -1,20 +1,18 @@
 // see https://github.com/electron/electron/issues/18738
 exports.default = function(context) {
+  if (context.electronPlatformName === 'darwin') {
+    return;
+  }
   var fs = require('fs');
   var localeDir = context.appOutDir+'/locales/';
-  fs.readdir(localeDir, function(err, files){
-    if (err) {
-      throw err;
+  for (const file of fs.readdirSync(localeDir)) {
+    switch (file) {
+      case "en-US.pak":
+      case "ja.pak":
+        break;
+      default:
+        fs.unlinkSync(localeDir + file);
+        break;
     }
-    for (const file of files) {
-      switch (file) {
-        case "en-US.pak":
-        case "ja.pak":
-          break;
-        default:
-          fs.unlinkSync(localeDir + file);
-          break;
-      }
-    }
-  });
+  }
 }

3fd3c7af14f1278ec6601b0360d9a6c87374b767
Kubo, Ryosuke <sunfish-shogi@users.noreply.github.com>
2023-05-06 02:17:03 +0900

    add test

--- src/tests/mock/research.ts
+++ src/tests/mock/research.ts
@@ -4,5 +4,18 @@ import { usiEngineSetting } from "./usi";
 export const researchSetting: ResearchSetting = {
   usi: usiEngineSetting,
   enableMaxSeconds: false,
-  maxSeconds: 10,
+  maxSeconds: 5,
+};
+
+export const researchSettingMax5Seconds: ResearchSetting = {
+  usi: usiEngineSetting,
+  enableMaxSeconds: true,
+  maxSeconds: 5,
+};
+
+export const researchSettingSecondaryEngines: ResearchSetting = {
+  usi: usiEngineSetting,
+  secondaries: [{ usi: usiEngineSetting }, { usi: usiEngineSetting }],
+  enableMaxSeconds: false,
+  maxSeconds: 5,
 };

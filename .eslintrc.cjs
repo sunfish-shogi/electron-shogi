8b4fd442ef662e0f3b188315fb6c40483d4137f0
Kubo, Ryosuke <sunfish-shogi@users.noreply.github.com>
2023-11-23 19:25:25 +0900

    Set no-console rule as error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,8 +13,8 @@ module.exports = {
     "prettier",
   ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": "error",
+    "no-debugger": "error",
     "no-irregular-whitespace": "off",
     "vue/multi-word-component-names": "off",
   },

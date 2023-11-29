1201271cbdac5fd2c0680fc4b8ccdca0605b2837
Kubo, Ryosuke <sunfish-shogi@users.noreply.github.com>
2023-11-29 23:18:29 +0900

    Remove core module

--- src/common/helpers/string.ts
+++ src/common/helpers/string.ts
@@ -1,11 +1,3 @@
-export function appendLine(base: string, newLine: string): string {
-  return (base ? appendReturnIfNotExists(base) : "") + appendReturnIfNotExists(newLine);
-}
-
-export function appendReturnIfNotExists(str: string): string {
-  return str + (str.endsWith("\n") ? "" : "\n");
-}
-
 export function toString(data: unknown): string {
   return data instanceof Error
     ? data.message

dec262562d88c3de8840db046a4734c4e9181ec2
Kubo, Ryosuke <sunfish-shogi@users.noreply.github.com>
2024-03-04 00:45:06 +0900

    Add eslint-plugin-import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,9 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:vue/vue3-recommended",
     "@vue/typescript/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
     "@vue/prettier",
     "@vue/eslint-config-prettier",
     "@vue/eslint-config-typescript",
@@ -18,5 +21,11 @@ module.exports = {
     "no-restricted-imports": ["error", { patterns: ["../"] }],
     "no-irregular-whitespace": "off",
     "vue/multi-word-component-names": "off",
+    "import/no-cycle": 1,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+    },
   },
 };
